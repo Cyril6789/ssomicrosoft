479a4835b06a4764ab2fb00f9b82665f19fc59a1
Cyril6789 <ch_67_89@msn.com>
2025-06-18 11:11:52 +0200

    Implement Entra ID API integration

--- inc/sync.class.php
+++ inc/sync.class.php
@@ -24,11 +24,91 @@ class PluginSyncaadSync {
    }
 
    private static function fetchUsersFromEntra(array $conn) {
-      // TODO: implement real calls to Entra ID API
-      return [];
+      $token = self::getAccessToken($conn);
+      if (!$token) {
+         return [];
+      }
+
+      $url = 'https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail,userPrincipalName,givenName,surname,accountEnabled';
+
+      if (!empty($conn['email_filter'])) {
+         $filter = urlencode("endsWith(mail,'".$conn['email_filter']."')");
+         $url .= "&%24filter=" . $filter;
+      }
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, [
+         'Authorization: Bearer ' . $token,
+         'Content-Type: application/json'
+      ]);
+      $response = curl_exec($ch);
+      curl_close($ch);
+
+      if (!$response) {
+         return [];
+      }
+
+      $data = json_decode($response, true);
+      return $data['value'] ?? [];
+   }
+
+   private static function getAccessToken(array $conn) {
+      $url = 'https://login.microsoftonline.com/' . $conn['tenant_id'] . '/oauth2/v2.0/token';
+      $params = [
+         'client_id'     => $conn['client_id'],
+         'client_secret' => $conn['client_secret'],
+         'scope'         => 'https://graph.microsoft.com/.default',
+         'grant_type'    => 'client_credentials'
+      ];
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+      $resp = curl_exec($ch);
+      curl_close($ch);
+
+      if (!$resp) {
+         return false;
+      }
+
+      $token = json_decode($resp, true);
+      return $token['access_token'] ?? false;
    }
 
    private static function syncUser(array $user) {
-      // TODO: implement synchronization with GLPI users
+      global $DB;
+
+      if (empty($user['mail'])) {
+         return;
+      }
+
+      $iterator = $DB->request([
+         'SELECT' => 'id',
+         'FROM'   => 'glpi_users',
+         'WHERE'  => ['email' => $user['mail']]
+      ]);
+
+      if ($iterator && $iterator->numrows()) {
+         $row = $iterator->next();
+         $DB->update('glpi_users', [
+            'name'      => $user['userPrincipalName'],
+            'realname'  => $user['surname'] ?? '',
+            'firstname' => $user['givenName'] ?? '',
+            'is_active' => $user['accountEnabled'] ? 1 : 0
+         ], [
+            'id' => $row['id']
+         ]);
+      } else {
+         $DB->insert('glpi_users', [
+            'name'      => $user['userPrincipalName'],
+            'realname'  => $user['surname'] ?? '',
+            'firstname' => $user['givenName'] ?? '',
+            'email'     => $user['mail'],
+            'password'  => '',
+            'is_active' => $user['accountEnabled'] ? 1 : 0
+         ]);
+      }
    }
 }
